016ea6e547773b22aa5390be1b2cbcded3613573
Sebastian Hörl <hoerl.sebastian@gmail.com>
2024-02-09 18:35:55 +0100

    cleanup

--- src/main/java/org/sinfonica/inclusive_ccam/heterogenous_users/drt/UserSpecificStopTimeModule.java
+++ src/main/java/org/sinfonica/inclusive_ccam/heterogenous_users/drt/UserSpecificStopTimeModule.java
@@ -1,19 +1,17 @@
 package org.sinfonica.inclusive_ccam.heterogenous_users.drt;
 
-import org.matsim.contrib.drt.stops.ParallelStopTimeCalculator;
 import org.matsim.contrib.drt.stops.PassengerStopDurationProvider;
-import org.matsim.contrib.drt.stops.StopTimeCalculator;
 import org.matsim.contrib.dvrp.run.AbstractDvrpModeModule;
 
 public class UserSpecificStopTimeModule extends AbstractDvrpModeModule {
 
-    public UserSpecificStopTimeModule(String mode) {
-        super(mode);
-    }
+	public UserSpecificStopTimeModule(String mode) {
+		super(mode);
+	}
 
-    @Override
-    public void install() {
-        bindModal(PassengerStopDurationProvider.class).to(UserSpecificStopTimeProvider.class);
-    }
+	@Override
+	public void install() {
+		bindModal(PassengerStopDurationProvider.class).to(UserSpecificStopTimeProvider.class);
+	}
 
 }
